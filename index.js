fca8faeb9e3167fad031623e4e916023c7b3c384
Omar Amerah <omaramerah2@gmail.com>
2023-01-18 11:03:28 +0000

    First part

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const { User } = require('./db');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+const bcrypt = require('bcrypt')
+const SALT_COUNT =5
 
 app.get('/', async (req, res, next) => {
   try {
@@ -17,8 +19,39 @@ app.get('/', async (req, res, next) => {
 // POST /register
 // TODO - takes req.body of {username, password} and creates a new user with the hashed password
 
+app.post('/register', async (req, res, next) => {
+  try{
+    console.log(req.body)
+    const user = await req.body.username
+    const pass = await req.body.password
+    const hashedpass = await bcrypt.hash(pass, SALT_COUNT)
+    User.create({username: user, password: hashedpass})
+    res.sendStatus(200)
+  }
+ catch (error) {
+  console.error(error);
+  next(error)
+}
+})
+
+
 // POST /login
 // TODO - takes req.body of {username, password}, finds user by username, and compares the password with the hashed version from the DB
+
+app.post('/login', async (req, res, next) => {
+  try{
+    console.log(req.body)
+    const user = await req.body.username
+    const pass = await req.body.password
+    User.findOne({where: { username: user, password: pass }})
+    res.sendStatus(200)
+  }
+ catch (error) {
+  console.error(error);
+  next(error)
+}
+})
+
 
 // we export the app, not listening in here, so that we can run tests
 module.exports = app;
